45a6f828ee451ed219ffd1c9e76966e34b538cf8
rentzsch <jwr.git@redshed.net>
2026-04-02 10:23:16 -0600

    init

---
+++ Makefile
@@ -0,0 +1,35 @@
+BINNAME = native-instruction-support
+CCFLAGS = -std=c17 -Wall -Wextra -pedantic
+
+all: fmt build test clean
+
+fmt:
+	clang-format -i -style='{BasedOnStyle: LLVM, IndentPPDirectives: BeforeHash}' *.c
+
+ifeq ($(shell uname -s),Darwin)
+#
+# Apple Darwin (macOS): Utilize Rosetta 2 to test x64 in addition to arm64.
+#
+build:
+	cc $(CCFLAGS) -o $(BINNAME)-arm64 *.c
+	cc $(CCFLAGS) -arch x86_64 -o $(BINNAME)-x64 *.c
+
+test:
+	./$(BINNAME)-arm64
+	./$(BINNAME)-x64
+
+clean:
+	rm -f $(BINNAME)-arm64 $(BINNAME)-x64
+else
+#
+# Other OS: Just build and test the native architecture.
+#
+build:
+	cc $(CCFLAGS) -o $(BINNAME) *.c
+
+test:
+	./$(BINNAME)
+
+clean:
+	rm -f $(BINNAME)
+endif
---
+++ native-instruction-support.c
@@ -0,0 +1,104 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#if defined(__linux__)
+  #if defined(__aarch64__)
+    #include <asm/hwcap.h>
+    #include <sys/auxv.h>
+  #elif defined(__x86_64__) || defined(__i386__)
+    #include <cpuid.h>
+  #endif
+#elif defined(__APPLE__)
+  #include <sys/sysctl.h>
+
+static int sysctlbynameErr(const char *name, void *oldp, size_t *oldlenp,
+                           void *newp, size_t newlen) {
+  int err = sysctlbyname(name, oldp, oldlenp, newp, newlen);
+  if (err == -1) {
+    err = errno;
+    if (err != ENOENT) {
+      fprintf(stderr, "*** ERR sysctlbyname(\"%s\") => errno:%d strerror:%s\n",
+              name, err, strerror(err));
+      exit(EXIT_FAILURE);
+    }
+  }
+  return err;
+}
+#endif
+
+//---------------------------------------------------------------------
+//
+// main
+//
+
+int main(int argc __attribute__((unused)),
+         const char *argv[] __attribute__((unused))) {
+#if defined(__linux__)
+  const char *os = "Linux";
+  #if defined(__x86_64__) || defined(__i386__)
+  //-------------------------------------------------------------------
+  // Linux x86.
+
+  const char *arch = "x86";
+  bool hasNativeSHA1 = false;
+
+  // Check if leaf 7 is available
+  if (__get_cpuid_max(7, NULL) >= 7) {
+    unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
+    __cpuid_count(7, 0, eax, ebx, ecx, edx);
+    hasNativeSHA1 = (ecx & (1 << 1)) != 0;
+  }
+  #elif defined(__aarch64__)
+  //-------------------------------------------------------------------
+  // Linux arm64.
+
+  const char *arch = "arm64";
+  unsigned long hwcaps = getauxval(AT_HWCAP);
+  bool hasNativeSHA1 = (hwcaps & HWCAP_SHA1) != 0;
+  #else
+    #error Currently-Unsupported Linux ISA
+  #endif
+#elif defined(__APPLE__)
+  #if defined(__aarch64__)
+  //-------------------------------------------------------------------
+  // Apple Silicon.
+
+  const char *os = "Apple"; // macOS, iOS, tvOS, watchOS, visionOS, etc.
+  const char *arch = "arm64";
+
+  int supported = 0;
+  size_t len = sizeof(supported);
+  int err =
+      sysctlbynameErr("hw.optional.arm.FEAT_SHA1", &supported, &len, NULL, 0);
+  bool hasNativeSHA1 = !err && supported;
+  #elif defined(__x86_64__)
+  //-------------------------------------------------------------------
+  // Apple macOS Intel.
+
+  const char *os = "macOS";
+  const char *arch = "Intel";
+
+  char features[1024];
+  memset(features, 0, sizeof(features));
+  size_t size = sizeof(features);
+  int err =
+      sysctlbynameErr("machdep.cpu.leaf7_features", features, &size, NULL, 0);
+  bool hasNativeSHA1 = !err && strstr(features, "SHA") != NULL;
+  #else
+    #error Currently-Unsupported Apple ISA
+  #endif
+#endif
+
+  printf("{\n"
+         "  \"arch\": \"%s\",\n"
+         "  \"os\": \"%s\",\n"
+         "  \"hasNativeSHA1\": %s,\n"
+         "}\n",
+         arch, os, hasNativeSHA1 ? "true" : "false");
+
+  return EXIT_SUCCESS;
+}
